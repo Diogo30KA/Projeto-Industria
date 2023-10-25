9b0497629646560cc0f0c545c12fc6a80c56f8f9
Diogo <106183864+Diogo30KA@users.noreply.github.com>
2023-10-25 13:45:49 -0300

    Add files via upload

---
+++ industria (1).sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tempo de Geração: 25/10/2023 às 18h43min
+-- Versão do Servidor: 5.5.20
+-- Versão do PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `industria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `cod` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `telefone` int(10) NOT NULL,
+  PRIMARY KEY (`cod`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`cod`, `nome`, `endereco`, `cidade`, `estado`, `telefone`) VALUES
+(1, 'Ana', 'Centro', 'Criciuma', 'SC', 12345),
+(2, 'Carlos', 'Centro', 'Ararangua', 'SC', 54321);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionarios`
+--
+
+CREATE TABLE IF NOT EXISTS `funcionarios` (
+  `cod` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  PRIMARY KEY (`cod`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`cod`, `nome`, `login`, `senha`) VALUES
+(1, 'Cris', 'cris', '123'),
+(2, 'Luciano', 'lu', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
+--
+
+CREATE TABLE IF NOT EXISTS `pedidos` (
+  `cod` int(5) NOT NULL,
+  `datapedido` date NOT NULL,
+  `codfunc` int(5) NOT NULL,
+  `codcli` int(5) NOT NULL,
+  `codprod` int(5) NOT NULL,
+  `quantidade` int(5) NOT NULL,
+  `preco` float(8,2) NOT NULL,
+  PRIMARY KEY (`cod`),
+  KEY `codcli` (`codcli`),
+  KEY `codfunc` (`codfunc`),
+  KEY `codprod` (`codprod`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pedidos`
+--
+
+INSERT INTO `pedidos` (`cod`, `datapedido`, `codfunc`, `codcli`, `codprod`, `quantidade`, `preco`) VALUES
+(1, '2023-05-15', 1, 1, 1, 5, 3.00),
+(2, '2023-05-15', 2, 2, 2, 10, 30.00),
+(3, '2023-05-16', 1, 1, 3, 10, 2.00),
+(4, '2023-05-16', 2, 2, 1, 5, 3.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `cod` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `quantidade` int(5) NOT NULL,
+  `preco` float(8,2) NOT NULL,
+  PRIMARY KEY (`cod`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`cod`, `nome`, `quantidade`, `preco`) VALUES
+(1, 'caneta', 10, 3.00),
+(2, 'caderno', 20, 30.00),
+(3, 'borracha', 25, 3.00),
+(4, 'lapis', 50, 2.00);
+
+--
+-- Restrições para as tabelas dumpadas
+--
+
+--
+-- Restrições para a tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`codfunc`) REFERENCES `funcionarios` (`cod`),
+  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`codprod`) REFERENCES `produtos` (`cod`),
+  ADD CONSTRAINT `pedidos_ibfk_3` FOREIGN KEY (`codcli`) REFERENCES `clientes` (`cod`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
